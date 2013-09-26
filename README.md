8825acbca3076b1006cb19987919afe83f3da267
Alan Shaw <alan@freestyle-developments.co.uk>
2013-09-26 12:15:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+chrome-tabs
+===
+
+[Browserify](http://browserify.org/) compatible [chrome-tabs](https://github.com/adamschwartz/chrome-tabs) by Adam Schwartz.
+
+Requirements
+---
+
+1. jQuery
+2. [brfs](https://github.com/substack/brfs) transform to inline HTML and CSS resources when browserified:
+
+```sh
+$ browserify -t brfs chrome-tabs.js > bundle.js
+```
+
+Usage
+---
+
+```javascript
+var chromeTabs = require("chrome-tabs")
+
+// Add chrome tabs CSS to page head
+chromeTabs.insertCss()
+
+// Init chrome tabs in the container .chrome-tabs-shell
+var $chromeTabsExampleShell = $(".chrome-tabs-shell")
+
+chromeTabs.init({
+  $shell: $chromeTabsExampleShell,
+  minWidth: 45,
+  maxWidth: 180
+})
+
+// Add a new tab
+chromeTabs.addNewTab($chromeTabsExampleShell, {
+  favicon: "http://g.etfv.co/https://www.hubspot.com",
+  title: "HubSpot",
+  data: {
+    timeAdded: +new Date()
+  }
+})
+
+// Log tab change data to console
+$chromeTabsExampleShell.bind("chromeTabRender", function (){
+  var $currentTab = $chromeTabsExampleShell.find(".chrome-tab-current")
+  if ($currentTab.length && window["console"] && console.log) {
+    console.log("Current tab index", $currentTab.index(), "title", $.trim($currentTab.text()), "data", $currentTab.data("tabData").data)
+  }
+})
+```
---
+++ chrome-tabs.js
@@ -0,0 +1,148 @@
+var fs = require('fs'),
+    insertCss = require('insert-css');
+
+var $ = jQuery;
+
+if (!$.ui.sortable) {
+  require('./jquery-ui-1.10.3.custom.js');
+}
+
+if (document.body.style['-webkit-mask-repeat'] !== void 0) {
+  $('html').addClass('cssmasks');
+} else {
+  $('html').addClass('no-cssmasks');
+}
+
+var tabTemplate = fs.readFileSync(__dirname + '/chrome-tabs.html');
+
+var defaultNewTabData = {
+  title: 'New Tab',
+  favicon: '',
+  data: {}
+};
+
+var chromeTabs = {
+  init: function(options) {
+    var render;
+    $.extend(options.$shell.data(), options);
+    options.$shell.find('.chrome-tab').each(function() {
+      return $(this).data().tabData = {
+        data: {}
+      };
+    });
+    render = function() {
+      return chromeTabs.render(options.$shell);
+    };
+    $(window).resize(render);
+    return render();
+  },
+  render: function($shell) {
+    chromeTabs.fixTabSizes($shell);
+    chromeTabs.fixZIndexes($shell);
+    chromeTabs.setupEvents($shell);
+    chromeTabs.setupSortable($shell);
+    return $shell.trigger('chromeTabRender');
+  },
+  setupSortable: function($shell) {
+    var $tabs;
+    $tabs = $shell.find('.chrome-tabs');
+    return $tabs.sortable({
+      axis: 'x',
+      tolerance: 'pointer',
+      start: function(e, ui) {
+        chromeTabs.fixZIndexes($shell);
+        if (!$(ui.item).hasClass('chrome-tab-current')) {
+          return $tabs.sortable('option', 'zIndex', $(ui.item).data().zIndex);
+        } else {
+          return $tabs.sortable('option', 'zIndex', $tabs.length + 40);
+        }
+      },
+      stop: function(e, ui) {
+        return chromeTabs.setCurrentTab($shell, $(ui.item));
+      }
+    });
+  },
+  fixTabSizes: function($shell) {
+    var $tabs, margin, width;
+    $tabs = $shell.find('.chrome-tab');
+    margin = (parseInt($tabs.first().css('marginLeft'), 10) + parseInt($tabs.first().css('marginRight'), 10)) || 0;
+    width = $shell.width() - 50;
+    width = (width / $tabs.length) - margin;
+    width = Math.max($shell.data().minWidth, Math.min($shell.data().maxWidth, width));
+    return $tabs.css({
+      width: width
+    });
+  },
+  fixZIndexes: function($shell) {
+    var $tabs;
+    $tabs = $shell.find('.chrome-tab');
+    return $tabs.each(function(i) {
+      var $tab, zIndex;
+      $tab = $(this);
+      zIndex = $tabs.length - i;
+      if ($tab.hasClass('chrome-tab-current')) {
+        zIndex = $tabs.length + 40;
+      }
+      $tab.css({
+        zIndex: zIndex
+      });
+      return $tab.data({
+        zIndex: zIndex
+      });
+    });
+  },
+  setupEvents: function($shell) {
+    $shell.unbind('dblclick').bind('dblclick', function() {
+      return chromeTabs.addNewTab($shell);
+    });
+    return $shell.find('.chrome-tab').each(function() {
+      var $tab;
+      $tab = $(this);
+      $tab.unbind('click').click(function() {
+        return chromeTabs.setCurrentTab($shell, $tab);
+      });
+      return $tab.find('.chrome-tab-close').unbind('click').click(function() {
+        return chromeTabs.closeTab($shell, $tab);
+      });
+    });
+  },
+  addNewTab: function($shell, newTabData) {
+    var $newTab, tabData;
+    $newTab = $(tabTemplate);
+    $shell.find('.chrome-tabs').append($newTab);
+    tabData = $.extend(true, {}, defaultNewTabData, newTabData);
+    chromeTabs.updateTab($shell, $newTab, tabData);
+    return chromeTabs.setCurrentTab($shell, $newTab);
+  },
+  setCurrentTab: function($shell, $tab) {
+    $shell.find('.chrome-tab-current').removeClass('chrome-tab-current');
+    $tab.addClass('chrome-tab-current');
+    return chromeTabs.render($shell);
+  },
+  closeTab: function($shell, $tab) {
+    if ($tab.hasClass('chrome-tab-current')) {
+      if ($tab.prev().length) {
+        chromeTabs.setCurrentTab($shell, $tab.prev());
+      } else if ($tab.next().length) {
+        chromeTabs.setCurrentTab($shell, $tab.next());
+      }
+    }
+    $tab.remove();
+    return chromeTabs.render($shell);
+  },
+  updateTab: function($shell, $tab, tabData) {
+    $tab.find('.chrome-tab-title').html(tabData.title);
+    $tab.find('.chrome-tab-favicon').css({
+      backgroundImage: "url('" + tabData.favicon + "')"
+    });
+    return $tab.data().tabData = tabData;
+  },
+  /**
+   * Insert chrome-tabs CSS into the page head
+   */
+  insertCss: function () {
+    insertCss(fs.readFileSync(__dirname + '/chrome-tabs.css'));
+  }
+};
+
+module.exports = chromeTabs;
